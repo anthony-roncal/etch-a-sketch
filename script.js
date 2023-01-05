b6325c426d29de102ac89eeb4a46192d3b781935
Anthony <acr1191@gmail.com>
2023-01-05 14:15:04 -0800

    add toggle to enable rainbow mode

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-let defaultGridSize = 16;
+let size = 16;
+let side = 600/size;
 const divGrid = document.querySelector('#grid');
 const btnReset = document.querySelector('#btn-reset');
 btnReset.addEventListener('click', createCustomGrid);
+const checkboxRainbow = document.querySelector('#checkbox-rainbow');
 
 function createGrid(size){
     for (let i = 0; i < Math.pow(size, 2); i++) {
         let square = document.createElement('div');
-        let side = 600/size;
+        side = 600/size;
         square.setAttribute('style', `width: ${side}px; height: ${side}px; border: solid 1px black; box-sizing: border-box;`);
         square.classList.add("square");
         divGrid.appendChild(square);
@@ -25,8 +27,11 @@ function createCustomGrid() {
 }
 
 function fillSquare(e) {
-    console.log(e.target.classList.add('hovered'));
+    let randomColor = Math.floor(Math.random()*0xFFFFFF).toString(16);
+    console.log(randomColor);
+    (checkboxRainbow.checked) ? e.target.setAttribute('style', `width: ${side}px; height: ${side}px; border: solid 1px black; box-sizing: border-box; background-color: #${randomColor}`) : 
+    e.target.setAttribute('style', `width: ${side}px; height: ${side}px; border: solid 1px black; box-sizing: border-box; background-color: #000000`);
 }
 
-createGrid(defaultGridSize);
+createGrid(size);
 
